282ca2d9017605f399ea8a306a2d5c9500679b36
Vinicius Dias <vdias6849@gmail.com>
2019-09-15 16:56:05 -0300

    delete em andamento

--- sisteminha_app/forms.py
+++ sisteminha_app/forms.py
@@ -3,7 +3,7 @@ from django import forms
 
 class ClienteForm(forms.Form):
     error_css_class = 'alert alert-danger'
-
+    idCliente = forms.CharField(widget= forms.widgets.Input(attrs ={'class':'form-control', 'placeholder':'Id do cliente'}))
     nome = forms.CharField(widget= forms.widgets.Input(attrs ={'class':'form-control', 'placeholder':'Nome'}))
     cpf = forms.CharField(widget= forms.widgets.Input(attrs ={'class':'form-control', 'placeholder':'CPF'}))
     cidade = forms.CharField(widget= forms.widgets.Input(attrs ={'class':'form-control', 'placeholder':'Cidade'}))
